122922570a514b11bf6999c9c21064f0eb11c5e4
Youssef-Boussaq <147155115+Youssef-Boussaq@users.noreply.github.com>
2023-11-20 23:29:29 +0100

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,19 @@
+public class Car {
+    String name;
+    int speed ;
+    String color ;
+
+    void star(){
+        System.out.println(name + " staring...") ;
+    }
+
+    void stop(){
+        System.out.println(name + " Stoping...") ;
+    }
+
+    void turn(){
+        System.out.println(name + " turnning...") ;
+    }
+
+
+}
--- Main.java
+++ Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Player fplay = new Player();
-        fplay.name = "Cr7" ;
-        fplay.age = 38 ;
-        fplay.rank = 1 ;
-        fplay.club = "Naser saudi arabic" ;
-        fplay.pass() ;
-        fplay.shoot() ;
-        fplay.jump() ;
+        Car car1 = new Car();
+        car1.name = "Audi" ;
+        car1.speed = 380 ;
+        car1.color = "black";
+        car1.star() ;
+        car1.stop() ;
+        car1.turn() ;
     }
 }
