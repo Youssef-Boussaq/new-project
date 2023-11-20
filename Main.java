a2f5e3cf70682ed40e1b917a6536973b44e3d6df
Youssef-Boussaq <147155115+Youssef-Boussaq@users.noreply.github.com>
2023-11-20 23:06:37 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Player fplay = new Player();
+        fplay.name = "Cr7" ;
+        fplay.age = 38 ;
+        fplay.rank = 1 ;
+        fplay.club = "Naser saudi arabic" ;
+        fplay.pass() ;
+        fplay.shoot() ;
+        fplay.jump() ;
+    }
+}
---
+++ Player.java
@@ -0,0 +1,20 @@
+public class Player {
+    String name;
+    int age ;
+    int rank ;
+    String club ;
+
+    void pass(){
+        System.out.println(name + " Passing...") ;
+    }
+
+    void shoot(){
+        System.out.println(name + " Shooting...") ;
+    }
+
+    void jump(){
+        System.out.println(name + " Jumping...") ;
+    }
+
+
+}
